28087df115b36fcedf5b3e53a37f729fe4f9e2e0
hugobullont <hugobullont@gmail.com>
2016-11-23 23:56:19 -0500

    Agregar archivos de proyecto.

---
+++ EncuestasWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EncuestasWeb.Startup))]
+namespace EncuestasWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
