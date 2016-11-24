0213c221870e273ba3534b80f118af1c3e4d646f
hugobullont <hugobullont@gmail.com>
2016-11-24 08:41:16 -0500

    Created

---
+++ EncuestasWeb/Controllers/TiposController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EncuestasWeb.Models;
+
+namespace EncuestasWeb.Controllers
+{
+    public class TiposController : Controller
+    {
+        private EncuestasUPCEntities db = new EncuestasUPCEntities();
+
+        // GET: Tipos
+        public ActionResult Index()
+        {
+            var tipos = db.Tipos.Include(t => t.Categoria);
+            return View(tipos.ToList());
+        }
+
+        // GET: Tipos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tipos tipos = db.Tipos.Find(id);
+            if (tipos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tipos);
+        }
+
+        // GET: Tipos/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoriaId = new SelectList(db.Categorias, "id", "NombreCategoria");
+            return View();
+        }
+
+        // POST: Tipos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,NombreTipo,CategoriaId")] Tipos tipos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tipos.Add(tipos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoriaId = new SelectList(db.Categorias, "id", "NombreCategoria", tipos.CategoriaId);
+            return View(tipos);
+        }
+
+        // GET: Tipos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tipos tipos = db.Tipos.Find(id);
+            if (tipos == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoriaId = new SelectList(db.Categorias, "id", "NombreCategoria", tipos.CategoriaId);
+            return View(tipos);
+        }
+
+        // POST: Tipos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,NombreTipo,CategoriaId")] Tipos tipos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tipos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoriaId = new SelectList(db.Categorias, "id", "NombreCategoria", tipos.CategoriaId);
+            return View(tipos);
+        }
+
+        // GET: Tipos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tipos tipos = db.Tipos.Find(id);
+            if (tipos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tipos);
+        }
+
+        // POST: Tipos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tipos tipos = db.Tipos.Find(id);
+            db.Tipos.Remove(tipos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
