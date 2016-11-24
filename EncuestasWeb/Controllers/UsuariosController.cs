2c58980e16ee6c7853f9ae82608d4fd5a4ed5238
hugobullont <hugobullont@gmail.com>
2016-11-24 12:27:06 -0500

    usuarios controller

---
+++ EncuestasWeb/Controllers/UsuariosController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EncuestasWeb.Models;
+
+namespace EncuestasWeb.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private EncuestasUPCEntities db = new EncuestasUPCEntities();
+
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            var usuarios = db.Usuarios.Include(u => u.Grupo);
+            return View(usuarios.ToList());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password)
+        {
+            Usuario user = GetUserByUsername(username);
+            if (user==null)
+            {
+                return HttpNotFound();
+            }
+            if (user.contraseña == password)
+            {
+                switch(user.Grupo.nombreGrupo)
+                {
+                    case "Analista":
+                        return RedirectToAction("Index");
+                    case "Administrador":
+                        return RedirectToAction("Index");
+                    case "Alumno":
+                        return RedirectToAction("Index");
+                    case "Docente":
+                        return RedirectToAction("Index");
+                    case "Administrativo":
+                        return RedirectToAction("Index");
+                }
+                          
+                
+            }
+            return HttpNotFound();
+        }
+
+        // GET: Usuarios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        
+        public Usuario GetUserByUsername(string userName)
+        {
+            Usuario usuario = (from u in db.Usuarios
+                               where u.codigo == userName
+                               select u).FirstOrDefault();
+            return usuario;
+        }
+
+        // GET: Usuarios/Create
+        public ActionResult Create()
+        {
+            ViewBag.GrupoId = new SelectList(db.Grupoes, "id", "nombreGrupo");
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,codigo,Nombre,GrupoId,ApellidoPaterno,ApellidoMaterno,contraseña")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usuarios.Add(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GrupoId = new SelectList(db.Grupoes, "id", "nombreGrupo", usuario.GrupoId);
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.GrupoId = new SelectList(db.Grupoes, "id", "nombreGrupo", usuario.GrupoId);
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,codigo,Nombre,GrupoId,ApellidoPaterno,ApellidoMaterno,contraseña")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GrupoId = new SelectList(db.Grupoes, "id", "nombreGrupo", usuario.GrupoId);
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Usuario usuario = db.Usuarios.Find(id);
+            db.Usuarios.Remove(usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
